d909bb7d474851db98652aa3dc11ab13c36535a8
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-16 12:37:18 -0300

    Java

--- Coding dojo/app/src/App.java
+++ Coding dojo/app/src/App.java
@@ -11,33 +11,33 @@ public class App {
         double descontoGasolinaAte20 = 0.04;
         double descontoGasolinaAcima20 = 0.06;
 
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite a quantidade de litros vendidos: ");
+            double litros = scanner.nextDouble();
 
-        System.out.print("Digite a quantidade de litros vendidos: ");
-        double litros = scanner.nextDouble();
+            System.out.print("Digite o tipo de combustível (A para Álcool, G para Gasolina): ");
+            char tipoCombustivel = scanner.next().charAt(0);
 
-        System.out.print("Digite o tipo de combustível (A para Álcool, G para Gasolina): ");
-        char tipoCombustivel = scanner.next().charAt(0);
+            double valorFinal = 0.0;
 
-        double valorFinal = 0.0;
-
-        if (tipoCombustivel == 'A') { 
-            if (litros <= 20) {
-                valorFinal = litros * precoAlcool * (1 - descontoAlcoolAte20);
+            if (tipoCombustivel == 'A') { 
+                if (litros <= 20) {
+                    valorFinal = litros * precoAlcool * (1 - descontoAlcoolAte20);
+                } else {
+                    valorFinal = litros * precoAlcool * (1 - descontoAlcoolAcima20);
+                }
+            } else if (tipoCombustivel == 'G') { 
+                if (litros <= 20) {
+                    valorFinal = litros * precoGasolina * (1 - descontoGasolinaAte20);
+                } else {
+                    valorFinal = litros * precoGasolina * (1 - descontoGasolinaAcima20);
+                }
             } else {
-                valorFinal = litros * precoAlcool * (1 - descontoAlcoolAcima20);
+                System.out.println("Tipo de combustível inválido.");
+                return;
             }
-        } else if (tipoCombustivel == 'G') { 
-            if (litros <= 20) {
-                valorFinal = litros * precoGasolina * (1 - descontoGasolinaAte20);
-            } else {
-                valorFinal = litros * precoGasolina * (1 - descontoGasolinaAcima20);
-            }
-        } else {
-            System.out.println("Tipo de combustível inválido.");
-            return;
+
+            System.out.printf("O valor a ser pago é: R$ %.2f\n", valorFinal);
         }
-
-        System.out.printf("O valor a ser pago é: R$ %.2f\n", valorFinal);
     }
 }
