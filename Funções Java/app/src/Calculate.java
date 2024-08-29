e9346d1461d5641ca652a7ee3fdf209848bccd93
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-29 17:19:10 -0300

    Calculadora

--- Funções Java/app/src/Calculate.java
+++ Funções Java/app/src/Calculate.java
@@ -1,5 +1,12 @@
 public class Calculate {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá");
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        sum(x, y);
+    }
+
+    static void sum(int x, int y){
+        System.out.println(x + y);
     }
 }
